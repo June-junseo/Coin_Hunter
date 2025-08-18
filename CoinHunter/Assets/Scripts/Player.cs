1f2ec3d59f8a933c829081ab9f7cdd94ebc3e5c2
June <heojunser@gmail.com>
2025-08-18 16:19:13 +0900

    Player 구현 / coin solid object 생성

---
+++ CoinHunter/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(-h, 0f, -v);
+
+        if (velocity.magnitude > 1f)
+        {
+            velocity.Normalize();
+        }
+
+        velocity *= speed;
+
+        body.linearVelocity = velocity;
+
+    }
+
+}
+
